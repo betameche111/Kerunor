0a14a01aba1df113911f32c610739812fc82fe6c
betameche111 <lolo.betameche@hotmail.fr>
2020-04-04 21:09:37 +0200

    Limit the paning zoom arround the map

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import {
-    WebGLRenderer,
-    Scene,
-    PerspectiveCamera
-} from 'three';
-import {
-    OrbitControls
-} from 'three/examples/jsm/controls/OrbitControls';
-import {
-    GLTFLoader
-} from 'three/examples/jsm/loaders/GLTFLoader';
+import { WebGLRenderer, Scene, PerspectiveCamera, MOUSE, Vector3 } from 'three';
+import { OrbitControls } from './OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import azerothMap from "./assets/gltf/map.gltf";
 
 var scene = new Scene();
@@ -21,21 +13,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x808080);
 document.body.appendChild(renderer.domElement);
 
-loader.load(azerothMap, function (gltf) {
+loader.load(azerothMap, function(gltf) {
     scene.add(gltf.scene);
-}, undefined, function (error) {
+}, undefined, function(error) {
     console.error(error);
 });
 
 camera.position.set(0, 1, 3);
 
+// Control Options
 controls.maxPolarAngle = Math.PI / 3;
 controls.autoRotate = true;
 controls.enableDamping = true;
-
+controls.maxDistance = 4;
+controls.minDistance = 1;
+controls.mouseButtons = {
+    LEFT: MOUSE.ROTATE,
+    MIDDLE: MOUSE.PAN,
+    RIGHT: MOUSE.PAN
+};
+controls.maxPan = new Vector3(1, 1, 1);
+controls.minPan = new Vector3(-1, -1, -1);
 controls.update();
 
-var animate = function () {
+var animate = function() {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
