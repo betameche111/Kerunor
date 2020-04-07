e85900ff9f6f21c968f73807569b6076d8d86e4e
Loïc Chacornac <lolo.betameche@hotmail.fr>
2020-04-07 16:46:51 +0200

    Limit the control

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { WebGLRenderer, Scene, PerspectiveCamera, MOUSE, Vector3, TextureLoader, RepeatWrapping, PlaneBufferGeometry, HemisphereLight, HemisphereLightHelper, DirectionalLight, DirectionalLightHelper } from 'three';
+import { WebGLRenderer, Scene, PerspectiveCamera, Fog, MOUSE, Vector3, TextureLoader, RepeatWrapping, PlaneBufferGeometry, HemisphereLight, HemisphereLightHelper, DirectionalLight, DirectionalLightHelper } from 'three';
 import { OrbitControls } from './OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Water } from 'three/examples/jsm/objects/Water.js';
@@ -10,7 +10,7 @@ import waterNormals from "./assets/img/waternormals.jpg";
 
 // Create Object
 var scene = new Scene();
-var camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 100, 100000);
+var camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1000, 110000);
 var loader = new GLTFLoader();
 var renderer = new WebGLRenderer();
 var controls = new OrbitControls(camera, renderer.domElement);
@@ -48,14 +48,18 @@ sky.material.uniforms["mieCoefficient"].value = 0.005;
 sky.material.uniforms["mieDirectionalG"].value = 0.8;
 sky.material.uniforms["sunPosition"].value = light.position.copy(light.position);
 
+
+//Fog control
+scene.fog = new Fog(0xE7E7E7, 1000, 100000);
+
 scene.add(sky);
 
-var waterGeometry = new PlaneBufferGeometry(100000, 100000);
+var waterGeometry = new PlaneBufferGeometry(2700000, 270000);
 
 var water = new Water(
     waterGeometry, {
-        textureWidth: 512,
-        textureHeight: 512,
+        textureWidth: 1024,
+        textureHeight: 1024,
         waterNormals: new TextureLoader().load(waterNormals, function(texture) {
             texture.wrapS = texture.wrapT = RepeatWrapping;
         }),
@@ -64,7 +68,7 @@ var water = new Water(
         sunColor: 0xffffff,
         waterColor: 0x001e0f,
         distortionScale: 3.7,
-        fog: scene.fog !== undefined
+        fog: scene.fog
     }
 );
 
@@ -73,7 +77,7 @@ scene.add(water);
 
 // Object Control
 loader.load(azerothMap, function(gltf) {
-    gltf.scene.scale.set(10, 10, 10);
+    gltf.scene.scale.set(100, 100, 100);
     scene.add(gltf.scene);
 }, function(xhr) {
     console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -82,27 +86,28 @@ loader.load(azerothMap, function(gltf) {
 });
 
 // Camera Control
-camera.position.set(0, 1000, 3000);
+camera.position.set(0, 10000, 30000);
 
 // Control Options
-controls.maxPolarAngle = Math.PI / 2.5;
+controls.maxPolarAngle = Math.PI / 3;
 // controls.autoRotate = true;
 controls.enableDamping = true;
-controls.maxDistance = 4000;
-controls.minDistance = 1000;
+controls.maxDistance = 30000;
+controls.minDistance = 15000;
 controls.mouseButtons = {
     LEFT: MOUSE.ROTATE,
     MIDDLE: MOUSE.PAN,
     RIGHT: MOUSE.PAN
 };
-controls.maxPan = new Vector3(1000, 1000, 1000);
-controls.minPan = new Vector3(-1000, -1000, -1000);
+controls.maxPan = new Vector3(7000, 7000, 7000);
+controls.minPan = new Vector3(-7000, -7000, -7000);
 controls.update();
 
 var animate = function() {
     controls.update();
     requestAnimationFrame(animate);
     water.material.uniforms['time'].value += 1.0 / 60.0;
+    camera.updateProjectionMatrix();
     renderer.render(scene, camera);
 };
 
