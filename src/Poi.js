2169f965fe31e8caaaf308c0a9b9f65a58f5d4a9
betameche111 <lolo.betameche@hotmail.fr>
2020-05-03 09:47:22 +0200

    update the poi template

--- src/Poi.js
+++ src/Poi.js
@@ -15,14 +15,16 @@ var Poi = [];
 
 var poi1 = new Sprite(spriteMaterial);
 poi1.position.set(1300, 1700, -5000);
-poi1.img = poi1img;
-poi1.img2 = poi2img
+poi1.img = [poi1img, poi2img];
+poi1.title = ["Poi 1 Titre 1", "Poi 1 Titre 2"];
+poi1.text = ["Je suis le texte 1", "Je suis le texte 2"];
 Poi.push(poi1);
 
 var poi2 = new Sprite(spriteMaterial);
 poi2.position.set(700, 900, 8000);
-poi2.img = poi2img;
-poi2.img2 = poi1img;
+poi2.img = [poi2img, poi1img];
+poi2.title = ["Poi 2 Titre 1", "Poi 2 Titre 2"];
+poi2.text = ["Je suis le texte 1", "Je suis le texte 2"];
 Poi.push(poi2);
 
 
@@ -32,10 +34,22 @@ Poi.forEach((element) => {
     element.unhover = () => element.material = spriteMaterial;
     element.renderOrder = 1;
     element.click = () => {
+        const modalElement = document.getElementById("modal-content");
+        modalElement.innerHTML = '<button class="uk-modal-close-full uk-close-large" type="button" style="background-color: rgba(255, 255, 255, 0.0);" uk-close></button>';
+        for (var i = 0; i < element.img.length; i++) {
+            modalElement.innerHTML = modalElement.innerHTML +
+                '<div class="uk-grid-collapse uk-flex-middle uk-height-viewport" uk-grid>' +
+                '<div class="uk-width-expand uk-flex-middle uk-flex">' +
+                '<img class="uk-padding-small" src="' + element.img[i] + '">' +
+                '</div>' +
+                '<div class="uk-padding-large uk-width-1-4 uk-light">' +
+                '<h1 id="modal-head">' + element.title[i] + '</h1>' +
+                '<p id="modal-text">' + element.text[i] + '</p>' +
+                '</div>' +
+                '</div>';
+        }
         UIkit.modal(document.getElementById("modal")).show();
-        document.getElementById("modal-img").setAttribute("data-src", element.img);
-        document.getElementById("modal-img-2").setAttribute("data-src", element.img2);
-    }
+    };
 });
 
 
